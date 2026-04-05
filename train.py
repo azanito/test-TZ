975a8238b17082d4cda6ca47dca40dffb580a6a6
azanito <azan.orunbek@gmail.com>
2026-04-05 15:31:28 +0500

    final

--- train.py
+++ train.py
@@ -133,7 +133,7 @@ def main():
         per_device_train_batch_size=args.per_device_train_batch_size,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         learning_rate=args.learning_rate,
-        fp16=True,
+        fp16=False,
         bf16=False,
         gradient_checkpointing=True,
         logging_steps=10,
